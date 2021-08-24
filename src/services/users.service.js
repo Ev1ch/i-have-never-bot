c4d5c152b1dba006d6b13e4ed55259d989439d7a
Vlad Zusko <vladzvu11@gmail.com>
2021-08-24 12:42:38 +0300

    Added users service

---
+++ src/services/users.service.js
@@ -0,0 +1,42 @@
+import { UsersRepository, UsersStatRepository } from '../repositories/index.js';
+
+class UsersService {
+    constructor() {
+        this.usersRepository = new UsersRepository();
+        this.usersStatRepository = new UsersStatRepository();
+    }
+
+    async create(id) {
+        const createdStat = await this.usersStatRepository.add();
+        const createdUser = await this.usersRepository.add({
+            id,
+            stat_id: createdStat.id,
+        });
+
+        return { ...createdUser, stat: createdStat };
+    }
+
+    async getStat(userId) {
+        const user = await this.usersRepository.getById(userId);
+
+        if (!user) {
+            return null;
+        }
+
+        const stat = await this.usersStatRepository.getById(user.stat_id);
+
+        return stat;
+    }
+
+    async updateStat(userId, stat) {
+        const user = await this.usersRepository.getById(userId);
+        const updatedStat = await this.usersStatRepository.updateById(
+            user.stat_id,
+            stat,
+        );
+
+        return updatedStat;
+    }
+}
+
+export default UsersService;
