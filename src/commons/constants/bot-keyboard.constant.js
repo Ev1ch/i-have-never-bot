309308895fabc77e5e3b988eaf98f7f721dd6dc8
Vlad Zusko <vladzvu11@gmail.com>
2021-08-24 12:40:57 +0300

    Added constants

---
+++ src/commons/constants/bot-keyboard.constant.js
@@ -0,0 +1,12 @@
+const BOT_KEYBOARD = [
+    [
+        {
+            text: 'Питання',
+        },
+        {
+            text: 'Питання 18+',
+        },
+    ],
+];
+
+export default BOT_KEYBOARD;
---
+++ src/commons/constants/index.js
@@ -0,0 +1,1 @@
+export { default as BOT_KEYBOARD } from './bot-keyboard.constant.js';
