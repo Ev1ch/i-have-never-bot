5b51014504cb0a6c9245ad9b2b097891bae5023b
Vlad Zusko <vladzvu11@gmail.com>
2021-08-24 12:42:53 +0300

    Added bot service

---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+export { default as BotService } from './bot.service.js';
+export { default as UsersService } from './users.service.js';
+export { default as QuestionsService } from './questions.service.js';
