72d7248f62d73c374ba250fd9e4f24927e76d8fa
Vlad Zusko <vladzvu11@gmail.com>
2021-09-03 14:15:46 +0300

    Added reserv port for telegram config

--- src/configs/telegram.config.js
+++ src/configs/telegram.config.js
@@ -2,6 +2,6 @@ import { config } from 'dotenv';
 config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT | 3000;
 
 export { BOT_TOKEN, PORT };
