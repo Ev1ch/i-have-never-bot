a6d09d16ca91f2cbfe58b01ac2a5841c0cb5ba0c
Vlad Zusko <vladzvu11@gmail.com>
2021-09-04 11:03:53 +0300

    Updated middleware

--- src/middlewares/log.middleware.js
+++ src/middlewares/log.middleware.js
@@ -1,9 +1,13 @@
+import { convertFromUnixTime } from '../helpers/index.js';
+
 async function logMiddleware(ctx, next) {
     const message = ctx.update.message;
     const user = message.from;
+    const time = convertFromUnixTime(message.date);
+
     const logMessage = `User: ${user.id}, ${user.username} => ${
         message.text
-    } | ${new Date(message.date).toUTCString()}`;
+    } | ${new Date(time).toUTCString()}`;
 
     console.log(logMessage);
 
