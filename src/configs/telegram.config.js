be48588d1efb4bbd6ebb706b81f2fc8a7b26c69e
Vlad Zusko <vladzvu11@gmail.com>
2021-09-04 10:52:03 +0300

    Added alternative url

--- src/configs/telegram.config.js
+++ src/configs/telegram.config.js
@@ -3,6 +3,6 @@ config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const PORT = process.env.PORT || 3000;
-const URL = process.env.APP_URL;
+const URL = process.env.APP_URL || '';
 
 export { BOT_TOKEN, PORT, URL };
